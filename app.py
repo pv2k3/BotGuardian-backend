d59a276f7fb8dc2221b15e21d514f9bf19d0e0bb
pv2k3 <vermapriyanshu001@gmail.com>
2025-02-19 21:31:08 +0530

    final vercel setup

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from pathlib import Path
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
+import uvicorn
 
 load_dotenv()
 
@@ -222,3 +223,6 @@ async def predict_user(username: str = Form(None)):
     except Exception as e:
         return {"error": str(e)}
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
