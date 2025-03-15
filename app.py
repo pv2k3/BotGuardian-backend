0f1f0cead4c4acf75ef382130395f1f920761efd
pv2k3 <vermapriyanshu001@gmail.com>
2025-03-15 12:35:51 +0530

    easy run

--- app.py
+++ app.py
@@ -131,6 +131,6 @@ async def predict_user(username: str = Form(None)):
     except Exception as e:
         return {"error": str(e)}
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
